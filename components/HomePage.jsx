1a5a330d98d2481f22ce2eacbaa1d3f2f77460d9
Antonio <antonio2@ualberta.ca>
2023-01-07 15:20:05 -0700

    Added Loading Bar w/ Text

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { auth } from "../firebase/firebaseApp";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TestDisplay from "./TestDisplay";
+import LoadingPage from "./LoadingPage";
 
 const HomePage = () => {
   const [user] = useAuthState(auth);
@@ -11,7 +12,6 @@ const HomePage = () => {
       <div className="w-screen h-screen flex flex-col justify-center items-center">
         <div className="text-center mb-6 text-sm bg-sky-100 p-3 rounded-md">
           <div className="mb-1 text-blue-500">Hello, {user?.displayName}</div>
-
           <button
             onClick={() => auth.signOut()}
             className="hover:underline text-red-500"

b93ea829d52d3c04c804eebea7584b2297dbbe1f
Jared Drueco <drueco03@gmail.com>
2023-01-07 15:44:11 -0700

    fixing conflict

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,6 +2,7 @@ import { auth } from "../firebase/firebaseApp";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TestDisplay from "./TestDisplay";
 import React, { useState } from "react";
+import LoadingPage from "./LoadingPage";
 
 const HomePage = () => {
   const [input, setInput] = useState("");

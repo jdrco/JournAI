997abb92b5b0e6a84446f86ab102cc8ed1ef48c0
Lynden Jones <lynden@Lyndens-MacBook-Pro.local>
2023-01-07 18:11:23 -0700

    Hardcoded D3 is hooked up

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import { auth } from "../firebase/firebaseApp";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TestDisplay from "./TestDisplay";
+import BarChart from "./BarChart";
+import { sort } from "d3";
 
 const HomePage = () => {
   const [user] = useAuthState(auth);
+
+  //Here for testing purposes with barchart element rigidity
+  let output = `[{"label": "sadness", "score": 0.4088817834854126}, {"label": "neutral", "score": 0.32471874356269836}, {"label": "surprise", "score": 0.13414035737514496}, {"label": "joy", "score": 0.03980706259608269}, {"label": "anger", "score": 0.034086793661117554}, {"label": "fear", "score": 0.03125927224755287}, {"label": "disgust", "score": 0.027105990797281265}]`;
+  const data = JSON.parse(output);
+
+  data.sort(function(a, b){
+    return a.label.localeCompare(b.label)
+  })
 
   return (
     <>
@@ -18,8 +28,8 @@ const HomePage = () => {
           >
             Sign Out
           </button>
-        </div>
-        <TestDisplay />
+        </div >
+        <BarChart data/>
       </div>
     </>
   );
